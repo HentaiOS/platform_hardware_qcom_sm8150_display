8e757d5bf4c6a3b3f3eaec4aafca07c1b583119e
Dyneteve <dyneteve@pixelexperience.org>
2020-04-12 16:54:37 +0300

    Allow building without the proprietary package

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_BOARD_PLATFORM),msmnile)
+ifeq ($(TARGET_BOARD_PLATFORM),msmnile sm6150)
 LOCAL_PATH := $(call my-dir)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
